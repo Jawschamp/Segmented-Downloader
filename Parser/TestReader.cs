29de9832ffe7c317b323e3dea0ef17412af765ab
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-08 06:19:49 -0700

    Add files via upload

---
+++ Parser/TestReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class SegmentedDownloaderParser
+{
+    public static int get_number_of_segments(string seg_url)
+    {
+        int len = seg_url.Length - 9;
+        string remove_usless_data = seg_url.Remove(len);
+        int number_of_segments = Convert.ToInt32(remove_usless_data.Split("seg-")[1]);
+        return number_of_segments;
+    }
+}
