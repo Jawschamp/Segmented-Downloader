c89196b059a0557cc2c780834602256a72404b7e
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-08 17:32:41 -0700

    Learning some things

--- Parser/TestReader.cs
+++ Parser/TestReader.cs
@@ -1,12 +1,24 @@
-﻿using System;
-
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 class SegmentedDownloaderParser
 {
-    public static int get_number_of_segments(string seg_url)
+    public static async Task<string> segmented_HTTP_request_parse(string seg_url)
     {
-        int len = seg_url.Length - 9;
-        string remove_usless_data = seg_url.Remove(len);
+        var client = new HttpClient();
+        var result = await client.GetStringAsync(seg_url);
+
+        Console.WriteLine(result);
+        return seg_url;
+    }
+
+    public static async Task<int> get_number_of_segments()
+    {
+        // Not tested
+        string segs = segmented_HTTP_request_parse();
+        int len = segs.Length - 9;
+        string remove_usless_data = segs.Remove(len);
         int number_of_segments = Convert.ToInt32(remove_usless_data.Split("seg-")[1]);
         return number_of_segments;
     }
