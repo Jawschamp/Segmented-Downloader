cfa0cbc9adae62e4ea11b98d89fda1f55e566b40
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-11 13:42:24 -0700

    Update TestReader.cs

--- Parser/TestReader.cs
+++ Parser/TestReader.cs
@@ -10,34 +10,42 @@ namespace SegmentedDownloader
 
 
 
-        public async Task SegmentURIRequester(string segment_url)
+        public async Task<string> SegmentURIRequester(string segment_url)
         {
             var Client = new HttpClient();
             var request = await Client.GetAsync(segment_url);
             var response_status_code = request.StatusCode;
-            await SegmentSegNumParse(segment_url: segment_url);
-            //return response;
+            return "OK";
         }
 
 
-        public async Task<int> SegmentSegNumParse(string segment_url)
+        public async Task<string> SegmentNumParser(string segment_url)
         {
             int len = segment_url.Length - 9;
             string remove_useless_data = segment_url.Remove(len);
             int num_of_segs = Convert.ToInt32(remove_useless_data.Split("seg-")[1]);
             Console.WriteLine(remove_useless_data);
             Console.WriteLine(num_of_segs);
-            return num_of_segs;
+            return remove_useless_data;
         }
 
-
+        public async Task<string> SemgentURIBuilder(string segment_incomplete_url)
+        {
+            var incomplete_url = await SegmentNumParser(segment_url: segment_incomplete_url);
+            string complete_url = incomplete_url + "-v1-a1.ts";
+            return complete_url;
+        }
         public async Task<string> SegmentURIAlgorithm(string segment_url)
         {
             // feed SegmentURIRequester the segment_url that needs to be parsed
             // The first step is to request it 100 times per return of HTTP: 200 
             // Note: evetually have a time algorithm that is able to reasoabbly 
             // request the closest (highest segment count without getting a HTTP: 404)
-            await SegmentURIRequester(segment_url: segment_url);
+
+
+            // Algorithm
+
+
             return null;
         }
 
