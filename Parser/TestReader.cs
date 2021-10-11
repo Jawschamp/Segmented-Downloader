0398357e2d42dbf240f010176c960ea4c163f939
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-10 20:32:54 -0700

    Getting ready for a mininum working version that is actually helpful

--- Parser/TestReader.cs
+++ Parser/TestReader.cs
@@ -9,24 +9,13 @@ namespace SegmentedDownloader
         public int SegCount { get; private set; }
 
 
-        public async Task<int> SegmentParse()
-        {
-            SegCount = 0;
-            return SegCount;
-        }
 
-
-        public async Task SegmentURISegAlg(string segment_url)
+        public async Task SegmentURIRequester(string segment_url)
         {
             var Client = new HttpClient();
-            var response = await Client.GetAsync(segment_url);
-            if (response.IsSuccessStatusCode)
-            {
-                while (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine(await SegmentSegNumParse(segment_url: segment_url));
-                }
-            }
+            var request = await Client.GetAsync(segment_url);
+            var response_status_code = request.StatusCode;
+            await SegmentSegNumParse(segment_url: segment_url);
             //return response;
         }
 
@@ -36,10 +25,21 @@ namespace SegmentedDownloader
             int len = segment_url.Length - 9;
             string remove_useless_data = segment_url.Remove(len);
             int num_of_segs = Convert.ToInt32(remove_useless_data.Split("seg-")[1]);
+            Console.WriteLine(remove_useless_data);
             Console.WriteLine(num_of_segs);
             return num_of_segs;
         }
 
-        public async Task<> 
+
+        public async Task<string> SegmentURIAlgorithm(string segment_url)
+        {
+            // feed SegmentURIRequester the segment_url that needs to be parsed
+            // The first step is to request it 100 times per return of HTTP: 200 
+            // Note: evetually have a time algorithm that is able to reasoabbly 
+            // request the closest (highest segment count without getting a HTTP: 404)
+            await SegmentURIRequester(segment_url: segment_url);
+            return null;
+        }
+
     }
 }
