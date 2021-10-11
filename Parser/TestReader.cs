52b4e34a144654867b3fd2c2d3fdeebcf7ba4a68
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-10 18:37:22 -0700

    Adding some stuff again (it'll work tonight)

--- Parser/TestReader.cs
+++ Parser/TestReader.cs
@@ -2,24 +2,44 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-class SegmentedDownloaderParser
+namespace SegmentedDownloader
 {
-    public static async Task<string> segmented_HTTP_request_parse(string seg_url)
+    public class SegmentParser
     {
-        var client = new HttpClient();
-        var result = await client.GetStringAsync(seg_url);
+        public int SegCount { get; private set; }
 
-        Console.WriteLine(result);
-        return seg_url;
-    }
 
-    public static async Task<int> get_number_of_segments()
-    {
-        // Not tested
-        string segs = segmented_HTTP_request_parse();
-        int len = segs.Length - 9;
-        string remove_usless_data = segs.Remove(len);
-        int number_of_segments = Convert.ToInt32(remove_usless_data.Split("seg-")[1]);
-        return number_of_segments;
+        public async Task<int> SegmentParse()
+        {
+            SegCount = 0;
+            return SegCount;
+        }
+
+
+        public async Task SegmentURISegAlg(string segment_url)
+        {
+            var Client = new HttpClient();
+            var response = await Client.GetAsync(segment_url);
+            if (response.IsSuccessStatusCode)
+            {
+                while (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(await SegmentSegNumParse(segment_url: segment_url));
+                }
+            }
+            //return response;
+        }
+
+
+        public async Task<int> SegmentSegNumParse(string segment_url)
+        {
+            int len = segment_url.Length - 9;
+            string remove_useless_data = segment_url.Remove(len);
+            int num_of_segs = Convert.ToInt32(remove_useless_data.Split("seg-")[1]);
+            Console.WriteLine(num_of_segs);
+            return num_of_segs;
+        }
+
+        public async Task<> 
     }
 }
