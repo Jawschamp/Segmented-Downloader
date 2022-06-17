6c8f832f34de9eec524834489e458d26e1103b29
Jaws <38185563+Jawschamp@users.noreply.github.com>
2022-06-16 17:44:35 -0700

    Delete Python directory

--- Python/Parser/SegmentParser.py
+++
@@ -1,9 +0,0 @@
-from Python.SegmentedDownloader import SegmentDownloader
-
-
-class SegmentParser(SegmentDownloader):
-    def url_parser(self):
-        array = []
-        for i in range(1, 9999):
-            array.append(self.url.split("seg-")[0] + "seg-" + str(i) + "-v1-a1.ts")
-        return array
